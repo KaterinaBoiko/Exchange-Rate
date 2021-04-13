3c67bb3bbc26e3ab0b6d0c809ec86308692a4a72
Kateryna Boiko <katerina.boiko16@gmail.com>
2021-04-14 00:50:07 +0300

    added world rate

---
+++ backend/parsers/currency-layer.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+const formatDate = require('dateformat');
+const sql = require('../database/connection');
+
+const date = formatDate(new Date(), "yyyy-mm-dd");
+(function (date) {
+    axios.get(`http://api.currencylayer.com/historical?access_key=d53eddc8bbd6f17586d4d83faa6a8740&date=${date}`)
+        .then(response => {
+            const { data } = response;
+            const uah = data.quotes.USDUAH;
+            for (pair in data.quotes) {
+                const currency = pair.substr(3);
+                const rate = data.quotes[pair];
+                if (currency && rate && uah) {
+                    sql.query(`select id from currency_pairs where base_currency='UAH' and currency = '${currency}'`, (err, result) => {
+                        if (err || !result.rowCount)
+                            return;
+
+                        sql.query(`update exchange_rates set world_rate = ${uah / rate} where currency_pair_id = ${result.rows[0].id} and date = '${date}'`, (err) => {
+                            if (err)
+                                return console.log(err);
+                        });
+                    });
+                }
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+})(date);
--- backend/parsers/mono.js
+++ backend/parsers/mono.js
@@ -9,14 +9,17 @@ const date = formatDate(new Date(), "yyyy-mm-dd");
             const { data } = response;
 
             data.forEach(pair => {
-                let { currencyCodeA, rateBuy, rateSell, rateCross } = pair;
+                let { currencyCodeA, currencyCodeB, rateBuy, rateSell, rateCross } = pair;
+                if (currencyCodeB !== 980)
+                    return;
+
                 sql.query(`select id from currency_pairs where base_currency='UAH' and currency = (select code from currencies where number_code = ${currencyCodeA})`, (err, result) => {
                     if (err || !result.rowCount)
                         return;
 
                     rateSell = rateSell ? rateSell : rateCross;
                     rateBuy = rateBuy ? rateBuy : rateCross;
-                    sql.query(`update exchange_rates set (sale_mono, purchase_mono) = (${rateSell ? rateSell : 'null'}, ${rateBuy ? rateBuy : 'null'}) where currency_pair_id = ${result.rows[0].id} and date = '${date}'`, (err, result) => {
+                    sql.query(`update exchange_rates set (sale_mono, purchase_mono) = (${rateSell ? rateSell : 'null'}, ${rateBuy ? rateBuy : 'null'}) where currency_pair_id = ${result.rows[0].id} and date = '${date}'`, (err) => {
                         if (err)
                             return console.log(err);
                     });
